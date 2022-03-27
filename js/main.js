81f3d4c35b8b02db3df3169f2cceeaa32330209e
Cama2109 <dadamirzayeva02@inbox.ru>
2022-03-27 18:12:54 +0500

    add filter part

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 var elButton = document.querySelectorAll(".btn");
+var elBox =document.querySelectorAll(".filter-box");
 console.log(elButton);
- for(var i=0; i<elButton.length; i++)
- {
+ for(var i=0; i<elButton.length; i++) {
      elButton[i].addEventListener("click",function(){
          for(var j=0; j < elButton.length; j++){
              elButton[j].classList.remove("active");
              this.classList.add("active");
          }
-     })
- }
+         var dataFilter = this.getAttribute("data-name");
+for(var k=0; k < elBox.length; k++){
+    elBox[k].classList.add("hide");
+    elBox[k].classList.remove("show");
 
+    if(dataFilter === elBox[k].getAttribute("data-item") || dataFilter ==="all") {
+        elBox[k].classList.add("show");
+        elBox[k].classList.remove("hide");
+    }
+}
+     })
+ 
+    }
